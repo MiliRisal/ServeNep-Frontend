5c59a0de2730544f419b98bc0b5b6469d4fc4853
[verdxnt-14] <shahiveronica99@gmail.com>
2021-07-06 16:32:00 +0545

    login binding

--- app/src/main/java/com/example/servenep/UI/LoginActivity.kt
+++ app/src/main/java/com/example/servenep/UI/LoginActivity.kt
@@ -2,10 +2,22 @@ package com.example.servenep.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.servenep.R
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var etemail: EditText
+    private lateinit var etpassword: EditText
+    private lateinit var btnlogin: Button
+    private lateinit var forgetpassword: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        etemail=findViewById(R.id.etemail)
+        etpassword=findViewById(R.id.etpassword)
+        btnlogin=findViewById(R.id.btnlogin)
+        forgetpassword=findViewById(R.id.forgetpassword)
     }
 }
