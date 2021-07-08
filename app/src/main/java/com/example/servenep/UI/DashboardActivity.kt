05b4d8e659588cb83e42602c5f301fec71b9516b
[verdxnt-14] <shahiveronica99@gmail.com>
2021-07-08 20:57:18 +0545

    icon binding

--- app/src/main/java/com/example/servenep/UI/DashboardActivity.kt
+++ app/src/main/java/com/example/servenep/UI/DashboardActivity.kt
@@ -2,11 +2,20 @@ package com.example.servenep.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.servenep.R
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var homeicon:ImageView
+    private lateinit var profileicon:ImageView
+    private lateinit var toolsicon: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        homeicon=findViewById(R.id.homeicon)
+        profileicon=findViewById(R.id.profileicon)
+        toolsicon=findViewById(R.id.toolsicon)
+
+
     }
 }
