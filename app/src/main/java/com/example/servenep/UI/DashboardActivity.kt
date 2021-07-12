b80713dba6d389fec2f12ee778f7c0ae18438a10
[verdxnt-14] <shahiveronica99@gmail.com>
2021-07-12 21:54:05 +0545

    menu

--- app/src/main/java/com/example/servenep/UI/DashboardActivity.kt
+++ app/src/main/java/com/example/servenep/UI/DashboardActivity.kt
@@ -2,8 +2,11 @@ package com.example.servenep.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.GridView
 import android.widget.ImageView
+import android.widget.Toast
 import com.example.servenep.Category
 import com.example.servenep.CategoryAdapter
 import com.example.servenep.R
@@ -30,6 +33,21 @@ class DashboardActivity : AppCompatActivity() {
         gridView?.adapter=CategoryAdapter
 
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.abc->Toast.makeText(applicationContext,"abc clicked",Toast.LENGTH_SHORT)
+            R.id.cde->Toast.makeText(applicationContext,"cde clicked",Toast.LENGTH_SHORT)
+            R.id.fgh->Toast.makeText(applicationContext,"fgh clicked",Toast.LENGTH_SHORT)
+            R.id.ijk->Toast.makeText(applicationContext,"ijk clicked", Toast.LENGTH_SHORT)
+
+        }
+        return super.onOptionsItemSelected(item)
     }
     private fun setDataList():ArrayList<Category>{
         var arrayList:ArrayList<Category> = ArrayList()
