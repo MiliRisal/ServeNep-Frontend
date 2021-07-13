a0a6eb07b6ef250efec356762e1470a7ff978894
[verdxnt-14] <shahiveronica99@gmail.com>
2021-07-13 22:02:09 +0545

    "Tasker Recycler"

---
+++ app/src/main/java/com/example/servenep/Adapter/TaskerAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.servenep.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.servenep.R
+import com.example.servenep.model.Tasker
+import de.hdodenhof.circleimageview.CircleImageView
+
+class TaskerAdapter ()
+//    val listTasker: ArrayList<Tasker>,
+//    val context: Context
+
+//        ): RecyclerView.Adapter<TaskerAdapter.TaskerViewHolder>(){
+//    class TaskerViewHolder(view: View): RecyclerView.ViewHolder(view){
+//        val imgProfile: CircleImageView
+//        val tvName: TextView
+//        val tvCategory: TextView
+//        val tvArea: TextView
+//        val tvPrice: TextView
+//
+//        init {
+//            imgProfile= view.findViewById(R.id.imgProfile)
+//            tvName=view.findViewById(R.id.tvName)
+//            tvCategory=view.findViewById(R.id.tvCategory)
+//            tvArea=view.findViewById(R.id.tvArea)
+//            tvPrice=view.findViewById(R.id.tvPrice)
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskerViewHolder {
+//        val view= LayoutInflater.from(Parent.context)
+//            .inflate(R.layout.tasker, parent, false)
+//        return TaskerViewHolder(view)
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return  listTasker.size
+//
+//    }
+//
+//    override fun onBindViewHolder(holder: TaskerViewHolder, position: Int) {
+//        val tasker= listTasker[position]
+//        holder.tvName.text=tasker.TaskerName
+//        holder.tvCategory.text=tasker.TaskerCategory
+//        holder.tvArea.text=tasker.TaskerArea
+//        holder.tvPrice.text=tasker.TaskerPrice.toString()
+//
+//        Glide.with(context)
+//            .load(tasker.TaskerImage)
+//            .into(holder.imgProfile)
+//    }
+//}
+
