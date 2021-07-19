194d68dbe3b324ba0d5d2405929d099e76351cd6
MiliRisal <milirisal11@gmail.com>
2021-07-19 16:55:31 +0545

    Tasker specification completed successfully

---
+++ app/src/main/java/com/example/servenep/entities/TaskerSpecification.kt
@@ -0,0 +1,8 @@
+package com.example.servenep.entities
+
+data class TaskerSpecification(
+    val name : String? = null,
+    val price : Int? = null,
+    val area : String? = null,
+    val category : String? = null
+)
---
+++ app/src/main/java/com/example/servenep/response/SpecificationResponse.kt
@@ -0,0 +1,6 @@
+package com.example.servenep.response
+
+data class SpecificationResponse (
+    val success: Boolean?= null,
+    val message : String? = null
+)
